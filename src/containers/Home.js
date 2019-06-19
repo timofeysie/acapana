358f1e417c8fbd1593a621d73c870f11bbf3ae49
Timothy Curchod <timofeyc@hotmail.com>
2019-06-20 07:07:56 +1000

    Added header links

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Acapana</h1>
+          <p>Create a list of WikiData</p>
+        </div>
+      </div>
+    );
+  }
+}
